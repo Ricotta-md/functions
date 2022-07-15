72bbc1ee4689bb1f6de1debe904918de09437458
Ran Huo <huoran2805263@outlook.com>
2022-07-15 23:55:17 +1000

    updated the helloworld() test function

--- tests/test_warmup.py
+++ tests/test_warmup.py
@@ -2,6 +2,7 @@ from functions.warmup import haversine,hello_world
 
 def test_length_of_hello_world():
     assert len(hello_world()) != 0
+    assert type(hello_world())==str
 
 def test_distance():
     assert round(haversine(48.865,2.380,48.235,2.393),2)==70.01
